3622a6ba38c61008122768c52121fa681dbdbd96
Shashwat <shashwatcha10@gmail.com>
2024-09-06 13:13:54 +0530

    Update table rendering to display row data in separate columns

--- bitcoin-frontend/src/App.js
+++ bitcoin-frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/data')
+    axios.get('http://localhost:5000/data')
       .then(response => {
         setData(response.data);
       })
@@ -21,14 +21,33 @@ function App() {
         <thead>
           <tr>
             <th>Page Number</th>
-            <th>Row Data</th>
+            <th>Address</th>	
+            <th>Balance</th>
+            <th>% of coins</th>
+            <th>First In</th>
+            <th>Last In</th>
+            <th>Ins</th>	
+            <th>First Out</th>
+            <th>Last Out</th>
+            <th>Outs</th>
           </tr>
         </thead>
         <tbody>
           {data.map((item, index) => (
             <tr key={index}>
               <td>{item.page_number}</td>
-              <td>{item.row_data.join(', ')}</td>
+              {item.row_data.map((dataPoint, dataIndex) => (
+                <td key={dataIndex}>{dataPoint}</td>
+              ))}
+              <td>{item.address}</td>
+              <td>{item.balance}</td>
+              <td>{item.percentage_of_coins}</td>
+              <td>{item.first_in}</td>
+              <td>{item.last_in}</td>
+              <td>{item.ins}</td>
+              <td>{item.first_out}</td>
+              <td>{item.last_out}</td>
+              <td>{item.outs}</td>
             </tr>
           ))}
         </tbody>
@@ -38,3 +57,4 @@ function App() {
 }
 
 export default App;
+
